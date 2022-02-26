ab48b34652aa5c3ad2773016c5e168948f448047
Terrence Pugh <tpughnsu@gmail.com>
2022-02-26 16:07:12 -0500

    sample test for github action

---
+++ tests/hello_test.py
@@ -0,0 +1,10 @@
+import unittest
+from hello import Greeter
+
+class MyTestCase(unittest.TestCase):
+    def test_default_greeting_set(self):
+        greeter = Greeter()
+        self.assertEqual(greeter.message, 'Hello world!')
+
+if __name__ == '__main__':
+    unittest.main()
